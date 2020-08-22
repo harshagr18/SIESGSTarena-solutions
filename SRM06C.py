2c58a1a9310bace33f285f81728171c87502991d
harshagr18 <harsh.agrawal17@siesgst.ac.in>
2020-08-22 14:39:24 +0530

    Trial SRM06C

--- SRM06C.py
+++ SRM06C.py
@@ -1,4 +1,4 @@
-def swapPositions(list, pos1, pos2): 
+def swapPositions(list, pos1, pos2): # Swapping algo
       
     list[pos1], list[pos2] = list[pos2], list[pos1] 
     return list
@@ -8,31 +8,27 @@ for o in range(u):
     count = 0
     left = []
     right = []
-
     a = int(input())
-
     intercept = [0 for i in range(a)]
 
-    for i in range(a):
+    for i in range(a):  # 2 Lists for Input
         x,y = list(map(int,input().split()))
         left.append(x)
         right.append(y)
 
-    for i in range(a-1):
+    for i in range(a-1): # Sorting right, and swapping left similarly
         for j in range(a-1):
             if right[j] > right[j+1]:
                 swapPositions(left, j, j+1)
                 swapPositions(right, j, j+1)
-    print("left =",left)
-    print("right =",right)
 
-    for i in range(a):
+    for i in range(a): # Main Algo
         if intercept[i] == 0:
+            j = i+1
             while j < a:
                 if left[j] < right[i]:
                     intercept[j] = 1
                 j = j+1
             intercept[i] = 1
             count = count+1
-            print("increasing count at j and i equal to",j,i)
     print(count)
