9eda2985bef1c97f95aa7262af187a7e2edb24db
Vipul Singh Raghuvanshi <singh.vipul15@siesgst.ac.in>
2020-08-25 16:07:50 +0530

    Update SRM06C.py
    comboList

--- SRM06C.py
+++ SRM06C.py
@@ -1,32 +1,21 @@
-def swapPositions(list, pos1, pos2): # Swapping algo
-      
-    list[pos1], list[pos2] = list[pos2], list[pos1] 
-    return list
-
 u = int(input())
 for o in range(u):
     count = 0
-    left = []
-    right = []
+    comboList = []
     a = int(input())
     intercept = [0 for i in range(a)]
 
-    for i in range(a):  # 2 Lists for Input
-        x,y = list(map(int,input().split()))
-        left.append(x)
-        right.append(y)
+    for i in range(a):  # comboList containing both left and right
+        comboList.append(list(map(int,input().split())))
 
-    for i in range(a-1): # Sorting right, and swapping left similarly
-        for j in range(a-1):
-            if right[j] > right[j+1]:
-                swapPositions(left, j, j+1)
-                swapPositions(right, j, j+1)
+    # Sorting comboList wrt right
+    comboList.sort(key=lambda x:x[1])
 
     for i in range(a): # Main Algo
         if intercept[i] == 0:
             j = i+1
             while j < a:
-                if left[j] <= right[i]:
+                if comboList[j][0] <= comboList[i][1]:
                     intercept[j] = 1
                 j = j+1
             intercept[i] = 1
