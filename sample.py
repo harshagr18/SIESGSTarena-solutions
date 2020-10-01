7c54902a4644db66a6a2fb90298c22e703c4f469
Aadityanand <aaditya.anand16@siesgst.ac.in>
2020-10-02 00:22:30 +0530

    would help in adding files

--- sample.py
+++ sample.py
@@ -5,3 +5,13 @@ for _ in range(u):
     a = int(input())
     a,b = list(map(int, input().split(" ")))
     a = list(map(int,input().split()))
+
+
+
+# Open function to open the file "MyFile1.txt"  
+# (same directory) in append mode and 
+file1 = open("MyFile.txt","a") 
+  
+# store its reference in the variable file1  
+# and "MyFile2.txt" in D:\Text in file2 
+file2 = open(r"D:\Text\MyFile2.txt","w+")
